bfd10400e13332463ef08ebcd17568f4f86865ec
wasaw <merenkovalexander@gmail.com>
2022-04-29 11:08:12 +0300

    Создан проект

---
+++ qrCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qrCode
+//
+//  Created by Александр Меренков on 4/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
